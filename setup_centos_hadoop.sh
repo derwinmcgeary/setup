6f607d8c10724ab440d05d5e35bc8ab777af0874
Derwin McGeary <derwinmcgeary+web@gmail.com>
2016-04-04 17:51:52 +0100

    add setup script for hadoop on Centos 6

---
+++ setup_centos_hadoop.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Simple setup.sh for configuring CentOS6 and Hadoop
+
+service iptables stop
+chkconfig iptables off
+service ip6tables stop
+chkconfig ip6tables off
+sysctl -w vm.swappiness=0
+echo 0 > /proc/sys/vm/swappiness
+ulimit -n 10000
+
+yum -y install ntp
+ntpdate 0.centos.pool.ntp.org
+service ntpd start
+ntpstat
+cd /opt
+
+wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u77-b03/jdk-8u77-linux-x64.rpm -O jdk-8u77-linux-x64.rpm
+
+rpm -ivh jdk-8u77-linux-x64.rpm
+
+alternatives --install /usr/bin/java java /usr/java/jdk1.8.0_77/bin/java
+200000
+
+wget -nv http://public-repo-1.hortonworks.com/ambari/centos6/2.x/updates/2.2.1.0/ambari.repo -O /etc/yum.repos.d/ambari.repo
+
+yum -y install mysql-connector-java
+yum -y install ambari-agent
